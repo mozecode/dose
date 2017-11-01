61e236a882e5c067a5fd510b88632bd45ec16e28
Greta Moseley <greta.moseley@gmail.com>
2017-11-01 15:29:35 -0500

    add connections from welcome to my account summary and back

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,7 +5,7 @@ const router = Router();
 
 const { getUserDetails } = require('../controllers/userCtrl');
 
-router.get('/user/:id', getUserDetails);
+router.get('/user/:id', getUserDetails); //renders user_detail.pug (my account page)
 
 // router.put('/user/:id', updateUser);
 
