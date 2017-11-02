2d8d6a553b40c9cb9709d672d2e128d726488f62
Greta Moseley <greta.moseley@gmail.com>
2017-11-02 11:01:25 -0500

    add renderCreateScriptForm function and createScript.pug template and test

--- controllers/prescriptionCtrl.js
+++ controllers/prescriptionCtrl.js
@@ -6,7 +6,10 @@
 // router.put('/prescription/:id', updateScript);
 var moment = require('moment');
 
-
+module.exports.renderCreateScriptForm = (req, res, next) => {
+    console.log("hello from renderCreateScriptForm")
+    res.render('createScript', {});
+}
 //organizes a user's prescriptions by time to take:
 module.exports.getAllUserScripts = (req, res, next) => {
     const { user, allergy, prescription } = req.app.get('models');
@@ -21,8 +24,27 @@ module.exports.getAllUserScripts = (req, res, next) => {
         })
         .then((oneuser) => {
             let person = oneuser[0].dataValues
+            // for(var i=0; i<person.prescriptions.length; i++){
+            // //can I just do this in the pug?  nested for loop here and pass it in through the object?
+            //     let time1 = moment(person.prescriptions[i].dataValues.frequency1, ["HH:mm:ss"]).format("h:mm A");
+            //     console.log(time1);
+                
+            //     let time2 = moment(person.prescriptions[i].dataValues.frequency2, ["HH:mm:ss"]).format("h:mm A");
+            //     console.log(time2);
+
+            //     let time3 = moment(person.prescriptions[i].dataValues.frequency3, ["HH:mm:ss"]).format("h:mm A");
+            //     console.log(time3);
+
+            //     let time4 = moment(person.prescriptions[i].dataValues.frequency4, ["HH:mm:ss"]).format("h:mm A");
+            //     console.log(time4);
+
+            //     let time5 = moment(person.prescriptions[i].dataValues.frequency5, ["HH:mm:ss"]).format("h:mm A");
+            //     console.log(time5);
+            // }
+
+
             // let now = moment();//use moment to display only future doses for this day?
-            // console.log(now);
+                //convert times from 24 hour clock to regular clock times for display to patient
             res.render('script_list', {//render list of user's scripts organized by time to take?
                 person,                 //use case/switch statement in pug?
                 allergy: person.allergies, //get user allergies
@@ -33,3 +55,10 @@ module.exports.getAllUserScripts = (req, res, next) => {
             next(err);
         });
 };
+
+//posts a new prescription to the database
+// module.exports.postScript=()=>{
+
+// }
+
+
--- routes/prescriptionRoute.js
+++ routes/prescriptionRoute.js
@@ -4,12 +4,13 @@ const { Router } = require('express');
 const router = Router();
 
 //require in items from the controller (see router below)
-const { getAllUserScripts } = require('../controllers/prescriptionCtrl');
+const { getAllUserScripts, renderCreateScriptForm } = require('../controllers/prescriptionCtrl');
 
 router.get('/prescriptions/user/:id', getAllUserScripts);
 // router.get('/prescription/:id', getScriptDetails);
 // router.post('/prescriptions', postScript);
-// router.post('/prescriptions/create', renderCreateScriptForm)
+router.get('/prescriptions/create', renderCreateScriptForm);
 // router.put('/prescription/:id', updateScript);
+
 
 module.exports= router;
