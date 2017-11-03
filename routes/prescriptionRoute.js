c582b741631204d8ca409d07d22023e6da76c9e1
Greta Moseley <greta.moseley@gmail.com>
2017-11-03 11:22:08 -0500

    add update prescription functionality, updateList pug view, update one script view and test

--- routes/prescriptionRoute.js
+++ routes/prescriptionRoute.js
@@ -4,13 +4,14 @@ const { Router } = require('express');
 const router = Router();
 
 //require in items from the controller (see router below)
-const { getAllUserScripts, renderCreateScriptForm, postScript } = require('../controllers/prescriptionCtrl');
+const { getAllUserScripts, renderCreateScriptForm, postScript, getScripts, getScriptDetails, updateScript } = require('../controllers/prescriptionCtrl');
 
 router.get('/prescriptions/user/:id', getAllUserScripts);
-// router.get('/prescription/:id', getScriptDetails);
+router.get('/prescriptions/update', getScripts);
+router.get('/prescription/:id', getScriptDetails);
 router.post('/prescriptions', postScript);
 router.get('/prescriptions/create', renderCreateScriptForm);
-// router.put('/prescription/:id', updateScript);
+router.put('/prescription/:id', updateScript);
 
 
 module.exports= router;
